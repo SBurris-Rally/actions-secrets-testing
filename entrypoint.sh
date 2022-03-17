2019d78ad8d57fbd20cc8abcf4dccbab1a585691
stephen.burris <stephen.burris@rallyhealth.com>
2022-03-17 12:36:56 -0400

    updated echos.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ cd "$GITHUB_WORKSPACE"
 
 echo $(ls)
 
-echo "Hello $INPUT_WHO-TO-GREET"
+echo "Hello $INPUT_WHO_TO_GREET"
+echo "First $INPUT_FIRST_SECRET"
 time=$(date)
 echo ::set-output name=time::$time
