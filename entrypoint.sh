b12b6907029a01ffb1df18f78271fa69443bc954
stephen.burris <stephen.burris@rallyhealth.com>
2022-03-17 13:17:54 -0400

    attempt10

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 cd "tmp"
 
 echo "Hello $INPUT_WHO_TO_GREET"
-echo $INPUT_FIRST_SECRET > tmp.txt
+echo "$INPUT_FIRST_SECRET" > tmp.txt
 cat tmp.txt
 base64 tmp.txt
+echo "$INPUT_FIRST_SECRET" | base64
 echo "Finished"
-echo $INPUT_FIRST_SECRET | base64
 time=$(date)
 echo ::set-output name=time::$time
