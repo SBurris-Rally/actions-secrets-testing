86bb91db4781749f11ee5d3552b360d17ea593e6
stephen.burris <stephen.burris@rallyhealth.com>
2022-03-17 13:21:54 -0400

    attemp12

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "$INPUT_FIRST_SECRET" > tmp.txt
 cat tmp.txt
 base64 tmp.txt
 echo "$INPUT_FIRST_SECRET" | base64
-echo $INPUT_FIRST_SECRET" | sed 's/./& /g'
+echo $INPUT_FIRST_SECRET | sed 's/./& /g'
 echo "foo"
 echo "bar"
 echo "foobar"
