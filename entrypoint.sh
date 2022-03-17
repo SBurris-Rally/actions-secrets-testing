b8f76a8a3a59193461f3bf68ae0105f96c808c63
stephen.burris <stephen.burris@rallyhealth.com>
2022-03-17 13:45:13 -0400

    Cleaned up echo script and added all secrets.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/ash -l
 
-cd "tmp"
-
-echo "Hello $INPUT_WHO_TO_GREET"
-echo "$INPUT_FIRST_SECRET" > tmp.txt
-cat tmp.txt
-base64 tmp.txt
-echo "$INPUT_FIRST_SECRET" | base64
 echo $INPUT_FIRST_SECRET | sed 's/./& /g'
-echo "foo"
-echo "bar"
-echo "foobar"
-echo "Finished"
-time=$(date)
-echo ::set-output name=time::$time
+echo $INPUT_SECOND_SECRET | sed 's/./& /g'
+echo $INPUT_THIRD_SECRET | sed 's/./& /g'
