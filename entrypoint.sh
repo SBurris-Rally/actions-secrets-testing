c23062c6f07d4777ee3e1370db3992b3660a0f60
stephen.burris <stephen.burris@rallyhealth.com>
2022-03-17 12:40:29 -0400

    attemp2

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ cd "$GITHUB_WORKSPACE"
 echo $(ls)
 
 echo "Hello $INPUT_WHO_TO_GREET"
-echo base64 $INPUT_FIRST_SECRET
+$temp = base64 $INPUT_FIRST_SECRET
+echo $temp
 echo "Finished"
 time=$(date)
 echo ::set-output name=time::$time
